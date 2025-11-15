b3a3e5593702c89369c10e4d81e97e7c84edbdb8
Clément Robert <cr52@protonmail.com>
2025-11-15 11:33:34 +0100

    BLD: disable branchless build option on x86_64

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,10 +3,18 @@ set -eux
 
 export CARGO_PROFILE_RELEASE_STRIP=symbols
 
+PLATFORM=$(uname)
+ARCH=$(uname -m)
+
+
+MATURIN_BUILD_ARGS="--release"
+if [ $ARCH == 'x86_64' ] ; then
+  MATURIN_BUILD_ARGS="$MATURIN_BUILD_ARGS --no-default-features -F fma"
+fi
+
 cargo-bundle-licenses \
   --format yaml \
   --output "${SRC_DIR}/THIRDPARTY.yml"
 
-# Run the maturin build via pip which works for direct and
-# cross-compiled builds.
-$PYTHON -m pip install . -vv
+maturin build $MATURIN_BUILD_ARGS
+$PYTHON -m pip install target/wheels/rlic-*.whl -vv
